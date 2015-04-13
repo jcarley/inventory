8e0bbc2414a95a78566e6300058be0bb7cb72ec3
jcarley <jcarley@gettyimages.com>
2015-04-13 14:38:36 -0500

    Adds elasticsearch to the provisioning scripts.

---
+++ scripts/elasticsearch.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+fancy_echo() {
+  printf "\n%b\n" "$1"
+}
+
+debInst() {
+    dpkg-query -Wf'${db:Status-abbrev}' "$1" 2>/dev/null | grep -q '^i'
+}
+
+trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
+
+set -e
+
+
+if ! debInst 'elasticsearch'; then
+  fancy_echo "Installing & configuring Elasticsearch ..."
+    wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
+    echo 'deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main' | sudo tee /etc/apt/sources.list.d/elasticsearch.list
+
+    sudo apt-get update
+    sudo aptitude install -y openjdk-7-jdk openjdk-7-jre-headless
+    sudo apt-get -y install elasticsearch=1.4.4
+    sudo update-rc.d elasticsearch defaults 95 10
+
+    sudo cp -f /home/vagrant/apps/inventory/scripts/files/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+    sudo service elasticsearch restart
+fi
+
