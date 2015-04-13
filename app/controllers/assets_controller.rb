f146a335f6ef1d232c24ec98f9c5ad94b52854b9
jcarley <jcarley@gettyimages.com>
2015-04-13 07:40:02 -0500

    Adds a response error formatter to be consistent with error responses.

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -11,12 +11,7 @@ class AssetsController < ApplicationController
     Domain.execute(cmd).on_success? do |result|
       render json: Asset.find(cmd.id)
     end.on_error? do |result|
-      render status: :unprocessable_entity,
-        json: {
-          success: false,
-          info: cmd.errors,
-          data: {}
-        }
+      render Lib::ResponseErrorFormatter.format(self, result.error)
     end
   end
 
@@ -25,10 +20,7 @@ class AssetsController < ApplicationController
     Domain.execute(cmd).on_success? do |result|
       render status: :no_content, json: Asset.find(asset_id)
     end.on_error? do |result|
-      render ResponseFormatter.format(self, result)
-      # formatter = ResponseFormatter.new(self)
-      # response_hash = formatter.format(result)
-      # render response_hash
+      render Lib::ResponseErrorFormatter.format(self, result.error)
     end
   end
 
@@ -45,19 +37,4 @@ class AssetsController < ApplicationController
   def asset_id
     params.require(:id)
   end
-      # if result.error.is_a?(NoBrainer::Error::DocumentNotFound)
-        # render status: :not_found,
-          # json: {
-            # success: false,
-            # info: result.error.message,
-            # data: {}
-          # }
-      # else
-        # render status: :unprocessable_entity,
-          # json: {
-            # success: false,
-            # info: result.error.message,
-            # data: {}
-          # }
-      # end
 end
---
+++ app/infrastructure/lib/response_error_formatter.rb
@@ -0,0 +1,37 @@
+module Lib
+  class ResponseErrorFormatter
+
+    def self.format(controller, error)
+      instance = new(controller)
+      instance.format(error)
+    end
+
+    def initialize(controller)
+      @controller = controller
+    end
+
+    def format(error)
+      if error.is_a?(NoBrainer::Error::DocumentNotFound)
+        {
+          status: :not_found,
+          json: {
+            success: false,
+            info: error.message,
+            data: {}
+          }
+        }
+      else
+        {
+          status: :unprocessable_entity,
+          json: {
+            success: false,
+            info: error.message,
+            data: {}
+          }
+        }
+      end
+    end
+
+  end
+end
+
