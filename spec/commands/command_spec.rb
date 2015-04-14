feb8d7a46f88871b4fdd996c8d316ef3df0509fb
jcarley <jcarley@gettyimages.com>
2015-04-13 23:42:05 -0500

    Adds a spec for the base command class.

---
+++ spec/commands/command_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Command do
+
+  describe "#run" do
+
+    context "when not valid" do
+      it "raises an CommandInvalidError" do
+        command = Command.new
+        allow(command).to receive(:valid?).and_return(false)
+        expect { command.run }.to raise_error(Command::CommandNotValidError)
+      end
+    end
+  end
+end
