03f629b0a1230a49c11a2545e383f1549e5baaa3
jcarley <jcarley@gettyimages.com>
2015-04-20 23:40:22 -0500

    Still working through some Grunt workflow stuff

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -6,6 +6,7 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
     project: {
+      name: ['inventory'],
       app: ['app'],
       assets: ['<%= project.app %>/assets'],
       css: ['<%= project.assets %>/sass/style.scss']
@@ -22,6 +23,24 @@ module.exports = function(grunt) {
       },
     },
 
+    copy: {
+      js: {
+        expand: true,
+        cwd: 'app/bower_components/',
+        src: [
+          'jquery/dist/jquery.js',
+          'angular/angular.js',
+          'angular-resource/angular-resource.js',
+          'angular-ui-router/release/angular-ui-router.js',
+          'angular-fontawesome/dist/angular-fontawesome.js',
+          'bootstrap-sass-official/assets/javascripts/bootstrap.js'
+        ],
+        dest: '<%= project.assets %>/tmp/',
+        flatten: true,
+        filter: 'isFile'
+      }
+    },
+
     ngAnnotate: {
       main: {
         src:'<%= project.assets %>/tmp/app.js',
@@ -32,12 +51,46 @@ module.exports = function(grunt) {
     uglify: {
       options: {
         screwIE8: true,
-        sourceMap: true
+        sourceMap: false,
+        mangle: false,
+        report: 'min'
       },
       main: {
         files: {
-          '<%= project.assets %>/javascripts/app.min.js': [
+          '<%= project.assets %>/tmp/app.min.js': [
             '<%= project.assets %>/tmp/app.js'
+          ]
+        }
+      },
+      vendor: {
+        files: {
+          '<%= project.assets %>/tmp/vendor.min.js': [
+            '<%= project.assets %>/tmp/jquery.js',
+            '<%= project.assets %>/tmp/angular.js',
+            '<%= project.assets %>/tmp/angular-resource.js',
+            '<%= project.assets %>/tmp/angular-ui-router.js',
+            '<%= project.assets %>/tmp/angular-fontawesome.js',
+            '<%= project.assets %>/tmp/bootstrap.js'
+          ]
+        }
+      }
+    },
+
+    concat: {
+      js: {
+        options: {
+          nonull: true,
+          sourceMap: true
+        },
+        files: {
+          '<%= project.assets %>/javascripts/app.min.js': [
+            '<%= project.assets %>/tmp/jquery.min.js',
+            '<%= project.assets %>/tmp/angular.min.js',
+            '<%= project.assets %>/tmp/angular-resource.min.js',
+            '<%= project.assets %>/tmp/angular-ui-router.min.js',
+            '<%= project.assets %>/tmp/angular-fontawesome.min.js',
+            '<%= project.assets %>/tmp/bootstrap.min.js',
+            '<%= project.assets %>/tmp/app.min.js'
           ]
         }
       }
@@ -46,28 +99,28 @@ module.exports = function(grunt) {
     sass: {
       dev: {
         options: {
-          style: 'expanded',
+          style: 'compressed',
           compass: false
         },
         files: {
-          '<%= project.assets %>/tmp/style.css':'<%= project.css %>'
+          '<%= project.assets %>/css/style.css':'<%= project.css %>'
         }
       }
     },
 
-    cssmin: {
-      options: {
-        shorthandCompacting: false,
-        roundingPrecision: -1
-      },
-      target: {
-        files: {
-          '<%= project.assets %>/css/style.css': [
-            '<%= project.assets %>/tmp/style.css'
-          ]
-        }
-      }
-    },
+    // cssmin: {
+      // options: {
+        // shorthandCompacting: false,
+        // roundingPrecision: -1
+      // },
+      // target: {
+        // files: {
+          // '<%= project.assets %>/css/style.css': [
+            // '<%= project.assets %>/tmp/style.css'
+          // ]
+        // }
+      // }
+    // },
 
     watch: {
       js: {
@@ -81,6 +134,8 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-coffee');
@@ -89,11 +144,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('cssbuild', [
-    'sass:dev', 'cssmin:target'
+    'sass:dev'
   ]);
 
   grunt.registerTask('jsbuild', [
-    'coffee:compileJoined', 'ngAnnotate', 'uglify'
+    'coffee:compileJoined', 'copy', 'ngAnnotate', 'uglify', 'concat:js'
   ]);
 
   grunt.registerTask('build', [
