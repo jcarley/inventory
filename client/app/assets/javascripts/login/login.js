fee99859058db91d32ec3a695fd630ab6934f35d
jcarley <jcarley@gettyimages.com>
2015-04-21 19:39:43 -0500

    Removing generated files

--- client/app/assets/javascripts/login/login.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  angular.module('Inventory').controller('LoginCtl', function($scope) {
-    return $scope.userNames = ['fflintstone', 'sjobs', 'bgates'];
-  });
-
-}).call(this);
-
-//# sourceMappingURL=login.js.map
--- client/app/assets/javascripts/main/main.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  angular.module('Inventory').controller('MainCtrl', function($scope) {
-    return $scope.awesomeThings = ['HTML5 Boilerplate', 'AngularJS', 'Karma', 'SitePoint', 'GoLang'];
-  });
-
-}).call(this);
-
-//# sourceMappingURL=main.js.map
