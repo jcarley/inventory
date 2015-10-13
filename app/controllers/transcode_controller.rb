4c7665ffefc081ad6fa61c14df51e763aac3799b
jcarley <jcarley@gettyimages.com>
2015-10-13 13:41:43 -0500

    Added command attributes and improved response messaging.

--- app/controllers/transcode_controller.rb
+++ app/controllers/transcode_controller.rb
@@ -2,11 +2,11 @@ class TranscodeController < ApplicationController
 
   def invoke
 
-    cmd = Processing::TranscodeCommand.new
+    cmd = Processing::TranscodeCommand.new(:id => "1234", :name => "Transcoder", :description => "Something useful")
 
     Domain.execute(cmd) do
-      is_success? { |result| render status: :ok, json: "{}" }
-      is_error? { |result| render status: :ok, json: "{}" }
+      is_success? { |result| render status: :ok, json: "{\"jid\":\"#{cmd.jid}\"}" }
+      is_error? { |result| render status: :ok, json: "{\"error\":\"#{result.error}\"}" }
     end
 
   end
