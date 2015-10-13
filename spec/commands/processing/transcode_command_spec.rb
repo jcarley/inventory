33eddc88dfc5851c8b9ae448c370dd0e82e8fb50
jcarley <jcarley@gettyimages.com>
2015-10-13 13:28:12 -0500

    Added a test to ensure the command gets executed

--- spec/commands/processing/transcode_command_spec.rb
+++ spec/commands/processing/transcode_command_spec.rb
@@ -11,6 +11,14 @@ RSpec.describe Processing::TranscodeCommand do
       }.to change(Processing::TranscodeCommand.jobs, :size).by(1)
     end
 
+    it "executes the command" do
+      cmd = Processing::TranscodeCommand.new(:id => "1234", :name => "Transcoder", :description => "Something useful")
+      Sidekiq::Testing.inline! do
+        Domain.execute(cmd)
+        puts cmd.jid
+      end
+    end
+
   end
 
 end
