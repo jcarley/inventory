211ee8f3e63884bc5fd452825e264155be1b9e5d
jcarley <jcarley@gettyimages.com>
2015-11-01 07:31:31 -0600

    Formatting

--- app/models/token_repository.rb
+++ app/models/token_repository.rb
@@ -12,17 +12,17 @@ class TokenRepository
     db.set(token.user_id, token.to_json)
   end
 
-    def delete_by(user_id)
-      token = self.find(user_id)
-      self.delete(token)
-    end
+  def delete_by(user_id)
+    token = self.find(user_id)
+    self.delete(token)
+  end
 
-    def delete(token)
-      db.delete(token.user_id)
-    end
+  def delete(token)
+    db.delete(token.user_id)
+  end
 
-    private
-    attr_reader :db
+  private
+  attr_reader :db
 
 end
 
